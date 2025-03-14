376ff74e19d7a5a9252cf72063574198c6639bfc
unknown <hahlbeck@kth.se>
2025-03-14 09:00:13 +0100

    inital train movement added to main, will switch to branch now

--- MetropedInteractive/Assets/Scripts/MovingTrains/Move.cs
+++ MetropedInteractive/Assets/Scripts/MovingTrains/Move.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     private float timer = 0f; 
-    private float moveInterval = 30f; 
+    private float returnInterval = 3f; 
     private MeshRenderer meshRenderer;
     Vector3 originalPos;
+    public Vector3 stopPos;
+
+    public float speed = 10f;
+    public float topSpeed = 10f;
+    public float accelleration = 0.04f;
+
+    private bool brake;
+    private bool isStationary = false;
+    public float timeBeforeOpen = 3;
+    public float timeWhileOpen = 3;
+    public float timeAfterClose = 3;
 
     void Start()
     {
@@ -20,17 +31,57 @@ public class Move : MonoBehaviour
     {
         if (transform.position.x < 170)
         {
-            transform.Translate(Vector3.right * 10 * Time.deltaTime); 
+            if (speed > 0 && brake) {
+                // Debug.Log("Braking");
+                speed -= accelleration;
+            } else if (speed <= 0 && brake && !isStationary) {
+                // Debug.Log("Stopped");
+                speed = 0;
+                StartStationary();
+            }
+
+            if(speed < topSpeed && !brake) {speed += accelleration;}
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
         else
         {
-            timer += Time.deltaTime; 
+            timer += Time.deltaTime;
 
-            if (timer >= moveInterval)
+            if (timer >= returnInterval)
             {
-                transform.position = originalPos;              
-                timer = 0f; 
+                transform.position = originalPos;
+                speed = topSpeed;
+                timer = 0f;
             }
         }
+    }
+
+    void StartStationary(){
+        if (!isStationary) {
+            isStationary = true;
+            StartCoroutine(StationaryTimer(()=>{brake = false; isStationary = false;}));
+        }
+    }
+
+    IEnumerator StationaryTimer(System.Action action) {
+        yield return new WaitForSeconds(timeBeforeOpen);
+        OpenDoors();
+        yield return new WaitForSeconds(timeWhileOpen);
+        CloseDoors();
+        yield return new WaitForSeconds(timeAfterClose);        
+        action?.Invoke();
+    }
+
+    void OpenDoors() {
+        Debug.Log("Opening Doors");
+    }
+
+    void CloseDoors() {
+        Debug.Log("Closing Doors");
+    }
+
+    void OnTriggerEnter(Collider collision) {
+        if (collision.CompareTag("BrakePoint")) {brake = true;}
+        Debug.Log("Breakpint hit");
     }
 }
